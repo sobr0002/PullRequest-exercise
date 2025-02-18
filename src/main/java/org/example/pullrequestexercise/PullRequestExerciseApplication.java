fb123dce18d4cfaa15e90d175cfed91c6e514575
Sofie Broman <sofierytter@hotmail.com>
2025-02-18 14:02:56 +0100

    hello

---
+++ src/main/java/org/example/pullrequestexercise/PullRequestExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.example.pullrequestexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pullrequestexercise/PullRequestExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pullrequestexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
